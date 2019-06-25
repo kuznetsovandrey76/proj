c01355a081ae5dc982cfd22c407ba60fc48099db
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-06-25 12:25:19 +0300

    update grid

--- src/js/scripts/custom.js
+++ src/js/scripts/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     
     // Magnific Popup
-    // var about_list = ['education', 'retraining', 'qual', 'conf', 'blog', 'olympiad', 'race'];
-    // $.each(about_list, function(index, value){
-    //     $('.popup-' + value).magnificPopup({
-    //         delegate: 'a',
-    //         type: 'image',
-    //         gallery: {
-    //             enabled: true,
-    //             navigateByImgClick: true,
-    //             preload: [0,1]
-    //         }
-    //     });
-    // });
+    var about_list = ['education', 'retraining', 'qual', 'conf', 'blog', 'olympiad', 'race'];
+    $.each(about_list, function(index, value){
+        $('.popup-' + value).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1]
+            }
+        });
+    });
 
     // Bootstrap carousel
     $('.carousel').carousel({
